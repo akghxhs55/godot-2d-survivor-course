7630846641ed152fea5334d22f4d265a4d090ef5
Jaeho Yi <akghxhs55@gmail.com>
2024-10-06 10:52:53 +0900

    Refine visual elements of ability upgrade card and upgrade screen
    - Improved layout and animations for `ability_upgrade_card.gd`.
    - Enhanced visual presentation and transitions in `upgrade_screen.gd`.

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -5,16 +5,53 @@ signal selected
 @onready var name_label: Label = $%NameLabel
 @onready var description_label: Label = $%DescriptionLabel
 
+var disabled: bool = false
+
 
 func _ready() -> void:
 	gui_input.connect(_on_gui_input)
+	mouse_entered.connect(_on_mouse_entered)
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade) -> void:
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
 
+
+func play_in(delay: float = 0) -> void:
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	$AnimationPlayer.play("in")
+	
+	
+func play_discard() -> void:
+	$AnimationPlayer.play("discard")
+	
 	
 func _on_gui_input(event: InputEvent) -> void:
+	if disabled:
+		return
+		
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		_select_card()
+		
+		
+func _on_mouse_entered() -> void:
+	if disabled:
+		return
+		
+	$HoverAnimationPlayer.play("hover")
+
+
+func _select_card() -> void:
+	disabled = true
+
+	$AnimationPlayer.play("selected")
+	
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card != self:
+			other_card.play_discard()
+
+	await $AnimationPlayer.animation_finished
+
+	selected.emit()
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -12,14 +12,19 @@ func _ready() -> void:
 
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+	var delay: float = 0.0
 	for upgrade in upgrades:
 		var card_instance: PanelContainer = upgrade_card_scenes.instantiate() as PanelContainer
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(delay)
 		card_instance.selected.connect(_on_upgrade_selected.bind(upgrade))
+		delay += 0.2
 		
 		
 func _on_upgrade_selected(upgrade: AbilityUpgrade) -> void:
 	upgrade_selected.emit(upgrade)
+	$AnimationPlayer.play("out")
+	await $AnimationPlayer.animation_finished
 	get_tree().paused = false
 	queue_free()
