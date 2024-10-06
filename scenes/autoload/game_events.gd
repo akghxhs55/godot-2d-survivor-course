5571f730e8bf74106c6ccb1225abf52e64d2cc0a
Jaeho Yi <akghxhs55@gmail.com>
2024-10-06 14:48:25 +0900

    Feat: Add vignette effect for player hit
          - Implemented vignette effect in `vignette.gd`.
          - Updated player hit effect to use vignette in `player.gd`.

--- scenes/autoload/game_events.gd
+++ scenes/autoload/game_events.gd
@@ -2,6 +2,7 @@ extends Node
 
 signal experience_vial_collected(number: float)
 signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrade: Dictionary)
+signal player_damaged
 
 
 func emit_experience_vial_collected(number: float) -> void:
@@ -10,3 +11,7 @@ func emit_experience_vial_collected(number: float) -> void:
 	
 func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrade: Dictionary) -> void:
 	ability_upgrade_added.emit(upgrade, current_upgrade)
+
+
+func emit_player_damaged() -> void:
+	player_damaged.emit()
---
+++ scenes/ui/vignette.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+
+func _ready() -> void:
+	GameEvents.player_damaged.connect(_on_player_damaged)
+	
+
+func _on_player_damaged() -> void:
+	$AnimationPlayer.play("hit")
+	
